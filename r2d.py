ef23da0cdec21ea36719d9ed420fd2456abdbe81
LenaG <mail@nomeans.net>
2013-12-01 22:54:57 -0800

    i have no idea what i am doing

--- r2d.py
+++ r2d.py
@@ -3,6 +3,8 @@ R2D- takes recipes and comments out the directions and serving size and makes in
 
 '''
 
+from fractions import Fraction
+
 r_name = "Vegetarian_Meatballs.txt"
 # assigned result of calling open to the variable f (file object)
 f = open(r_name)
@@ -11,14 +13,24 @@ f.close()
 
 recipe = dict()
 # all_lines is a list of strings
-for l in all_lines[:1]:
+for l in all_lines[:3]:
     print l
     # making a list of items contained in l, split when space occurs
     x = l.split()
+    # if line is empty continue
+    if len(x) == 0 : 
+        continue 
     # index into first value of x and make it into integer
-    amt = int(x[0])
-    ingredient = x[1]
-    measurement = "count"
+    print x, "is a listl"
+    amt = Fraction(x[0])
+    if len(x) == 2 :
+        ingredient = x[1]
+        measurement = "count"
+    # if measurement is there take is instead of defining as 'count' from indexing into the 3rd part of the list
+    else: 
+        measurement = x[1]
+        #pasha made me do this i have no idea what's going on, very lost
+        ingredient = " ".join(x[2:])
     recipe[ingredient] = [amt, measurement]
     
     print recipe
